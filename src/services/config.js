5acea447a4d3e895111b720652966eff8ae1c363
Stefan Thomas <justmoon@members.fsf.org>
2015-10-16 04:50:42 -0700

    [TASK] New config system, in-memory tests
    Run tests in memory (SQLite) - results in a roughly 2x speedup:
    
    ```
    npm test  5.96s user 0.38s system 56% cpu 11.216 total
    npm test  5.11s user 0.24s system 108% cpu 4.918 total
    ```

--- src/services/config.js
+++ src/services/config.js
@@ -1,42 +1,21 @@
 'use strict'
 
-const url = require('url')
 const tweetnacl = require('tweetnacl')
 const log = require('./log')('config')
+const Config = require('@ripple/five-bells-shared').Config
 
-const config = exports
+const config = module.exports = new Config('ledger')
 
-config.db = {}
-config.db.uri = process.env.DB_URI
-config.db.subspace = 'five-bells-ledger'
+config.parseServerConfig()
+config.parseDatabaseConfig()
 
-config.server = {}
-config.server.secure = !!process.env.PUBLIC_HTTPS
-config.server.bind_ip = process.env.BIND_IP || '0.0.0.0'
-config.server.port = process.env.PORT || 3000
-config.server.public_host = process.env.HOSTNAME || require('os').hostname()
-config.server.public_port = process.env.PUBLIC_PORT || config.server.port
-
-if (process.env.NODE_ENV === 'test') {
-  config.server.public_host = 'localhost'
-  config.db.subspace = 'five-bells-ledger-' + config.server.port
-} else if (process.env.NODE_ENV === 'unit') {
+if (process.env.NODE_ENV === 'unit') {
   config.server.public_host = 'localhost'
   config.server.port = 61337
   config.server.public_port = 80
-  config.db.subspace = 'five-bells-ledger-unit-test-' + process.pid
+  config.db.uri = 'sqlite://:memory:'
+  config.updateDerivativeServerConfig()
 }
-
-// Calculate base_uri
-const isCustomPort = config.server.secure
-  ? +config.server.public_port !== 443
-  : +config.server.public_port !== 80
-config.server.base_host = config.server.public_host +
-  (isCustomPort ? ':' + config.server.public_port : '')
-config.server.base_uri = url.format({
-  protocol: 'http' + (config.server.secure ? 's' : ''),
-  host: config.server.base_host
-})
 
 config.keys = {}
 config.keys.ed25519 = {
