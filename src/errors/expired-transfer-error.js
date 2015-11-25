18a9aebb97a26d29b608a22301f559139b28b0c6
Stefan Thomas <justmoon@members.fsf.org>
2015-11-25 11:28:44 -0800

    [TASK] Change to proper error class.

--- src/errors/expired-transfer-error.js
+++ src/errors/expired-transfer-error.js
@@ -1,23 +1,22 @@
 'use strict'
 
-const UnprocessableEntityError =
-require('five-bells-shared/errors/unprocessable-entity-error')
+const UnprocessableEntityError = require('five-bells-shared').UnprocessableEntityError
 
-module.exports = function ExpiredTransferError (message, accountIdentifier) {
-  Error.captureStackTrace(this, this.constructor)
-  this.name = this.constructor.name
-  this.message = message
-  this.accountIdentifier = accountIdentifier
-}
+class ExpiredTransferError extends UnprocessableEntityError {
+  constructor (message, accountIdentifier) {
+    super(message)
+    this.accountIdentifier = accountIdentifier
+  }
 
-require('util').inherits(module.exports, UnprocessableEntityError)
-
-module.exports.prototype.handler = function *(ctx, log) {
-  log.warn('Expired Transfer: ' + this.message)
-  ctx.status = 422
-  ctx.body = {
-    id: this.name,
-    message: this.message,
-    owner: this.accountIdentifier
+  * handler (ctx, log) {
+    log.warn('Expired Transfer: ' + this.message)
+    ctx.status = 422
+    ctx.body = {
+      id: this.name,
+      message: this.message,
+      owner: this.accountIdentifier
+    }
   }
 }
+
+module.exports = ExpiredTransferError
