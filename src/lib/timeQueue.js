922412e6d564c3a3259a2cd23b4228ccbc520286
Stefan Thomas <justmoon@members.fsf.org>
2015-11-25 10:43:17 -0800

    [TASK] Use ES6 classes for TimeQueue/TimeWorker.

--- src/lib/timeQueue.js
+++ src/lib/timeQueue.js
@@ -5,57 +5,59 @@ const moment = require('moment')
 const emitter = require('co-emitter')
 const PriorityQueue = require('priorityqueuejs')
 
-function TimeQueue () {
-  this._queue = new PriorityQueue(function (a, b) {
-    return b.date - a.date
-  })
-  emitter(this)
-}
-
-TimeQueue.prototype.insert = function *(date, item) {
-  const dateValue = moment(date).valueOf()
-  this._queue.enq({
-    date: dateValue,
-    item: item
-  })
-
-  // This event is used by the worker started in app.js
-  yield this.emit('insert', dateValue, item)
-}
-
-TimeQueue.prototype.getEarliestDate = function () {
-  if (this._queue.isEmpty()) {
-    return null
+class TimeQueue {
+  constructor () {
+    this._queue = new PriorityQueue(function (a, b) {
+      return b.date - a.date
+    })
+    emitter(this)
   }
-  return this._queue.peek().date
-}
 
-/**
- * Return all items that are due on or before the provided date.
- *
- * @param {Number} date Cutoff date
- * @return {Array} Queued items
- */
-TimeQueue.prototype.popBeforeDate = function (date) {
-  // pull modifies the original array
-  const dateValue = moment(date).valueOf()
-  const arrayItems = []
-  while (!this._queue.isEmpty() && this._queue.peek().date <= dateValue) {
-    arrayItems.push(this._queue.deq().item)
+  * insert (date, item) {
+    const dateValue = moment(date).valueOf()
+    this._queue.enq({
+      date: dateValue,
+      item: item
+    })
+
+    // This event is used by the worker started in app.js
+    yield this.emit('insert', dateValue, item)
   }
-  return arrayItems
-}
 
-TimeQueue.prototype.includes = function (item) {
-  return _.some(this._queue._elements, function (obj) {
-    return _.isEqual(obj.item, item)
-  })
-}
+  getEarliestDate () {
+    if (this._queue.isEmpty()) {
+      return null
+    }
+    return this._queue.peek().date
+  }
 
-TimeQueue.prototype.remove = function (item) {
-  _.remove(this._queue._elements, function (obj) {
-    return obj.item === item
-  })
+  /**
+   * Return all items that are due on or before the provided date.
+   *
+   * @param {Number} date Cutoff date
+   * @return {Array} Queued items
+   */
+  popBeforeDate (date) {
+    // pull modifies the original array
+    const dateValue = moment(date).valueOf()
+    const arrayItems = []
+    while (!this._queue.isEmpty() && this._queue.peek().date <= dateValue) {
+      arrayItems.push(this._queue.deq().item)
+    }
+    return arrayItems
+  }
+
+  includes (item) {
+    return _.some(this._queue._elements, function (obj) {
+      return _.isEqual(obj.item, item)
+    })
+  }
+
+  remove (item) {
+    _.remove(this._queue._elements, function (obj) {
+      return obj.item === item
+    })
+  }
 }
 
 exports.TimeQueue = TimeQueue
--- src/lib/timerWorker.js
+++ src/lib/timerWorker.js
@@ -5,59 +5,61 @@ const defer = require('co-defer')
 
 const MAX_32INT = 2147483647
 
-function TimerWorker (timeQueue, transferExpiryMonitor) {
-  this.timeQueue = timeQueue
-  this.transferExpiryMonitor = transferExpiryMonitor
-  this.timeout = null
-  this.listener = null
-}
-
-TimerWorker.prototype.start = function *() {
-  const _this = this
-
-  // Make sure we only have one listener waiting for new
-  // items to be added to the timeQueue
-  _this.listener = function *() {
-    yield _this.processTimeQueue()
-  }
-  _this.timeQueue.on('insert', _this.listener)
-
-  yield this.processTimeQueue()
-}
-
-TimerWorker.prototype.processTimeQueue = function *() {
-  const _this = this
-
-  // Process expired transfers
-  yield this.transferExpiryMonitor.processExpiredTransfers()
-
-  // Set the timer to the earliest date on the timeQueue
-  if (this.timeout) {
-    clearTimeout(this.timeout)
-  }
-  const earliestDate = this.timeQueue.getEarliestDate()
-
-  // Don't reschedule the timer if nothing is waiting
-  if (!earliestDate) {
-    return
+class TimerWorker {
+  constructor (timeQueue, transferExpiryMonitor) {
+    this.timeQueue = timeQueue
+    this.transferExpiryMonitor = transferExpiryMonitor
+    this.timeout = null
+    this.listener = null
   }
 
-  // If we set the timeout to greater than the MAX_32INT it
-  // will be triggered right away so we'll just set it to
-  // the longest possible timeout and that will cause us to check again
-  const timeoutDuration = Math.min(moment(earliestDate).diff(moment()), MAX_32INT)
-  this.timeout = defer.setTimeout(function *() {
-    yield _this.processTimeQueue()
-  }, timeoutDuration)
-}
+  * start () {
+    const _this = this
 
-TimerWorker.prototype.stop = function () {
-  const _this = this
+    // Make sure we only have one listener waiting for new
+    // items to be added to the timeQueue
+    _this.listener = function *() {
+      yield _this.processTimeQueue()
+    }
+    _this.timeQueue.on('insert', _this.listener)
 
-  clearTimeout(_this.timeout)
-  if (_this.listener) {
-    _this.timeQueue.off('insert', _this.listener)
-    _this.listener = null
+    yield this.processTimeQueue()
+  }
+
+  * processTimeQueue () {
+    const _this = this
+
+    // Process expired transfers
+    yield this.transferExpiryMonitor.processExpiredTransfers()
+
+    // Set the timer to the earliest date on the timeQueue
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+    const earliestDate = this.timeQueue.getEarliestDate()
+
+    // Don't reschedule the timer if nothing is waiting
+    if (!earliestDate) {
+      return
+    }
+
+    // If we set the timeout to greater than the MAX_32INT it
+    // will be triggered right away so we'll just set it to
+    // the longest possible timeout and that will cause us to check again
+    const timeoutDuration = Math.min(moment(earliestDate).diff(moment()), MAX_32INT)
+    this.timeout = defer.setTimeout(function *() {
+      yield _this.processTimeQueue()
+    }, timeoutDuration)
+  }
+
+  stop () {
+    const _this = this
+
+    clearTimeout(_this.timeout)
+    if (_this.listener) {
+      _this.timeQueue.off('insert', _this.listener)
+      _this.listener = null
+    }
   }
 }
 
