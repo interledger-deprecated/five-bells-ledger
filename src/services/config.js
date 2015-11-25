1698cfcf03147af4ab9d480199723374e6836324
Stefan Thomas <justmoon@members.fsf.org>
2015-11-25 11:29:45 -0800

    [TASK] Use shared key config feature.

--- src/services/config.js
+++ src/services/config.js
@@ -1,13 +1,12 @@
 'use strict'
 
-const tweetnacl = require('tweetnacl')
-const log = require('./log')('config')
 const Config = require('five-bells-shared').Config
 
 const config = module.exports = new Config('ledger')
 
 config.parseServerConfig()
 config.parseDatabaseConfig()
+config.parseKeyConfig()
 
 if (process.env.NODE_ENV === 'unit') {
   config.server.public_host = 'localhost'
@@ -15,30 +14,6 @@ if (process.env.NODE_ENV === 'unit') {
   config.server.public_port = 80
   config.db.uri = 'sqlite://:memory:'
   config.updateDerivativeServerConfig()
-}
-
-config.keys = {}
-config.keys.ed25519 = {
-  secret: process.env.ED25519_SECRET_KEY,
-  public: process.env.ED25519_PUBLIC_KEY
-}
-
-let keyPair
-if (!config.keys.ed25519.secret) {
-  log.warn('No ED25519_SECRET_KEY provided. Generating a random one. ' +
-    'DO NOT DO THIS IN PRODUCTION')
-  keyPair = tweetnacl.sign.keyPair()
-  config.keys.ed25519.secret = tweetnacl.util.encodeBase64(keyPair.secretKey)
-  config.keys.ed25519.public = tweetnacl.util.encodeBase64(keyPair.publicKey)
-}
-
-if (!config.keys.ed25519.public) {
-  if (!keyPair) {
-    keyPair = tweetnacl.sign.keyPair.fromSecretKey(
-      tweetnacl.util.decodeBase64(config.keys.ed25519.secret))
-  }
-  config.keys.ed25519.public =
-    tweetnacl.util.encodeBase64(keyPair.publicKey)
 }
 
 if (config.getEnv('ADMIN_USER')) {
