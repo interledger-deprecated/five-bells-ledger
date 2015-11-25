6afadfa6eab4025bdd37342a7350c23c36cb6d3f
Stefan Thomas <justmoon@members.fsf.org>
2015-11-25 10:53:04 -0800

    [TASK] Use ES6 class for TransferExpiryMonitor

--- src/lib/transferExpiryMonitor.js
+++ src/lib/transferExpiryMonitor.js
@@ -8,79 +8,81 @@ const updateState = require('./updateState')
 const ExpiredTransferError = require('../errors/expired-transfer-error')
 const Transfer = require('../models/transfer').Transfer
 
-function TransferExpiryMonitor (timeQueue, notificationWorker) {
-  this.queue = timeQueue
-  this.notificationWorker = notificationWorker
-}
-
-TransferExpiryMonitor.prototype.validateNotExpired = function (transfer) {
-  if (transfer.expires_at &&
-    moment().isAfter(transfer.expires_at)) {
-    throw new ExpiredTransferError('Cannot modify transfer ' +
-      'after expires_at date')
+class TransferExpiryMonitor {
+  constructor (timeQueue, notificationWorker) {
+    this.queue = timeQueue
+    this.notificationWorker = notificationWorker
   }
-}
 
-TransferExpiryMonitor.prototype.expireTransfer = function * (transferId) {
-  const _this = this
-
-  yield db.transaction(function *(transaction) {
-    let transfer = yield Transfer.findById(transferId, { transaction })
-
-    if (!transfer) {
-      log.error('trying to expire transfer that cannot be found ' +
-        'in the database: ' + transferId)
-      return
+  validateNotExpired (transfer) {
+    if (transfer.expires_at &&
+      moment().isAfter(transfer.expires_at)) {
+      throw new ExpiredTransferError('Cannot modify transfer ' +
+        'after expires_at date')
     }
-
-    if (!transfer.isFinalized()) {
-      updateState(transfer, 'rejected')
-      yield transfer.save({ transaction })
-
-      // Return the money to the original senders
-      let accountBalances = yield makeAccountBalances(transaction, transfer)
-      yield accountBalances.revertDebits()
-
-      log.debug('expired transfer: ' + transferId)
-
-      yield _this.notificationWorker.queueNotifications(transfer, transaction)
-    }
-  })
-
-  // Should process transfer state notifications soon, because some transfers
-  // may have changed state
-  this.notificationWorker.scheduleProcessing()
-}
-
-TransferExpiryMonitor.prototype.watch = function * (transfer) {
-  // Star the expiry countdown if we're not already watching it
-  if (!this.queue.includes(transfer.id)) {
-    const now = moment()
-    const expiry = moment(transfer.expires_at)
-    if (transfer.expires_at && now.isBefore(expiry)) {
-      yield this.queue.insert(expiry, transfer.id)
-
-      log.debug('transfer ' + transfer.id +
-        ' will expire in ' + expiry.diff(now, 'milliseconds') + 'ms')
-    }
-  } else if (transfer.state === 'executed' ||
-    transfer.state === 'rejected' ||
-    transfer.state === 'failed') {
-    this.unwatch(transfer.id)
   }
-}
 
-TransferExpiryMonitor.prototype.processExpiredTransfers = function *() {
-  log.debug('checking for transfers to expire')
-  const transfersToExpire = this.queue.popBeforeDate(moment())
-  for (let id of transfersToExpire) {
-    yield this.expireTransfer(id)
+  * expireTransfer (transferId) {
+    const _this = this
+
+    yield db.transaction(function *(transaction) {
+      let transfer = yield Transfer.findById(transferId, { transaction })
+
+      if (!transfer) {
+        log.error('trying to expire transfer that cannot be found ' +
+          'in the database: ' + transferId)
+        return
+      }
+
+      if (!transfer.isFinalized()) {
+        updateState(transfer, 'rejected')
+        yield transfer.save({ transaction })
+
+        // Return the money to the original senders
+        let accountBalances = yield makeAccountBalances(transaction, transfer)
+        yield accountBalances.revertDebits()
+
+        log.debug('expired transfer: ' + transferId)
+
+        yield _this.notificationWorker.queueNotifications(transfer, transaction)
+      }
+    })
+
+    // Should process transfer state notifications soon, because some transfers
+    // may have changed state
+    this.notificationWorker.scheduleProcessing()
   }
-}
 
-TransferExpiryMonitor.prototype.unwatch = function (transferId) {
-  log.debug('unwatch transfer: ' + transferId)
-  this.queue.remove(transferId)
+  * watch (transfer) {
+    // Star the expiry countdown if we're not already watching it
+    if (!this.queue.includes(transfer.id)) {
+      const now = moment()
+      const expiry = moment(transfer.expires_at)
+      if (transfer.expires_at && now.isBefore(expiry)) {
+        yield this.queue.insert(expiry, transfer.id)
+
+        log.debug('transfer ' + transfer.id +
+          ' will expire in ' + expiry.diff(now, 'milliseconds') + 'ms')
+      }
+    } else if (transfer.state === 'executed' ||
+      transfer.state === 'rejected' ||
+      transfer.state === 'failed') {
+      this.unwatch(transfer.id)
+    }
+  }
+
+  * processExpiredTransfers () {
+    log.debug('checking for transfers to expire')
+    const transfersToExpire = this.queue.popBeforeDate(moment())
+    for (let id of transfersToExpire) {
+      yield this.expireTransfer(id)
+    }
+  }
+
+  unwatch (transferId) {
+    log.debug('unwatch transfer: ' + transferId)
+    this.queue.remove(transferId)
+  }
 }
 
 exports.TransferExpiryMonitor = TransferExpiryMonitor
