290ace9b1bbd09616a7fdb5448ae418a825efa26
MatthewPhinney <matt.phinney@ripple.com>
2016-01-22 17:09:28 -0800

    Fix file path

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 /* @flow */
 'use strict'
 
-module.exports = require('./src/lib/app')
+module.exports = require('./lib/app')
 if (!module.parent) {
-  require('./src/services/app').start()
+  require('./services/app').start()
 }
