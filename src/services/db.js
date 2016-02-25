5b7217698c7377b69eaea0dc870503b2610499b5
MatthewPhinney <matt.phinney@ripple.com>
2016-02-25 11:46:18 -0800

    TEST add debug logging

--- src/services/db.js
+++ src/services/db.js
@@ -2,11 +2,11 @@
 
 const Sequelize = require('sequelize')
 const config = require('./config')
-const log = require('./log')('db')
+//const log = require('./log')('db')
 const DB = require('five-bells-shared').DB(Sequelize)
 var pg = require('pg')
 delete pg.native
 
 module.exports = new DB(config.getIn(['db', 'uri']), {
-  logging: log.debug
+  logging: console.log
 })
