e6d68c25b4c48419bc97bac516fab4acc6a66f3c
Stefan Thomas <justmoon@members.fsf.org>
2016-05-26 07:49:57 -0700

    chore: auto-update gh-pages

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -14,7 +14,7 @@ uploadCoverage() {
   fi
 }
 
-npmPublish() {
+publishNpm() {
   # Push NPM package if not yet published
   mv npmrc-env .npmrc
   if [ -z "$(npm info $(npm ls --depth=-1 2>/dev/null | head -1 | cut -f 1 -d " ") 2>/dev/null)" ]; then
@@ -22,7 +22,7 @@ npmPublish() {
   fi
 }
 
-dockerPush() {
+pushDocker() {
   # Push Docker image tagged latest and tagged with commit descriptor
   sed "s/<AUTH>/${DOCKER_TOKEN}/" < "dockercfg-template" > ~/.dockercfg
   docker tag interledger/five-bells-ledger:latest interledger/five-bells-ledger:"$(git describe)"
@@ -30,7 +30,12 @@ dockerPush() {
   docker push interledger/five-bells-ledger:"$(git describe)"
 }
 
+updateWebsite() {
+  node scripts/publish_web.js
+}
+
 # ST: Coverage reporting is broken - disable for now
 # uploadCoverage
-npmPublish
-dockerPush
+publishNpm
+pushDocker
+updateWebsite
