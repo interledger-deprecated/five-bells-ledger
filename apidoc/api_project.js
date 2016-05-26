7855978f06c3b6ea46cc3ce4164202bef6883400
CircleCI <info@circleci.com>
2016-05-26 18:44:52 +0000

    chore: update gh-pages

---
+++ apidoc/api_project.js
@@ -0,0 +1,27 @@
+define({
+  "name": "five-bells-ledger",
+  "version": "14.0.1",
+  "description": "Five Bells ledger reference implementation",
+  "title": "Five Bells Ledger API",
+  "url": "https://acme-ledger.example",
+  "order": [
+    "Transfer",
+    "PutTransfer",
+    "PutTransferFulfillment",
+    "GetTransfer",
+    "GetTransferFulfillment",
+    "Account",
+    "Subscription",
+    "PutSubscription",
+    "GetSubscription",
+    "Notification"
+  ],
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-05-26T18:44:51.861Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.2"
+  }
+});
