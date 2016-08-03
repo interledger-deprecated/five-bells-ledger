40730243c90816004dc83bbecce960209f98d402
CircleCI <info@circleci.com>
2016-08-03 02:35:58 +0000

    chore: update gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -20,7 +20,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-08-03T01:19:17.203Z",
+    "time": "2016-08-03T02:35:58.435Z",
     "url": "http://apidocjs.com",
     "version": "0.13.2"
   }
