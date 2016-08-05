a0c25c80afcb5e25e4c239543a3419a0abdb93ba
Ben Sharafian <benms64@gmail.com>
2016-08-05 16:45:43 +0200

    fix: use the app's config when creating metadata

--- src/controllers/metadata.js
+++ src/controllers/metadata.js
@@ -1,26 +1,4 @@
 'use strict'
-const config = require('../services/config')
-
-const base = config.getIn(['server', 'base_uri'])
-const metadata = {
-  currency_code: config.getIn(['currency', 'code']),
-  currency_symbol: config.getIn(['currency', 'symbol']),
-  condition_sign_public_key: config.getIn(['keys', 'ed25519', 'public']),
-  notification_sign_public_key: config.getIn(['keys', 'notification_sign', 'public']),
-  urls: {
-    health: base + '/health',
-    transfer: base + '/transfers/:id',
-    transfer_fulfillment: base + '/transfers/:id/fulfillment',
-    transfer_state: base + '/transfers/:id/state',
-    connectors: base + '/connectors',
-    accounts: base + '/accounts',
-    account: base + '/accounts/:name',
-    subscription: base + '/subscriptions/:id',
-    subscription_notification: base + '/subscriptions/:subscription_id/notifications/:notification_id'
-  },
-  precision: config.get('amount.precision'),
-  scale: config.get('amount.scale')
-}
 
 /**
  * @api {get} / Get Server Metadata
@@ -65,4 +43,28 @@ const metadata = {
 /*
  * @returns {void}
  */
-exports.getResource = function * () { this.body = metadata }
+module.exports = (config) => ({
+  getResource: function * () {
+    const base = config.getIn(['server', 'base_uri'])
+    const metadata = {
+      currency_code: config.getIn(['currency', 'code']),
+      currency_symbol: config.getIn(['currency', 'symbol']),
+      condition_sign_public_key: config.getIn(['keys', 'ed25519', 'public']),
+      notification_sign_public_key: config.getIn(['keys', 'notification_sign', 'public']),
+      urls: {
+        health: base + '/health',
+        transfer: base + '/transfers/:id',
+        transfer_fulfillment: base + '/transfers/:id/fulfillment',
+        transfer_state: base + '/transfers/:id/state',
+        connectors: base + '/connectors',
+        accounts: base + '/accounts',
+        account: base + '/accounts/:name',
+        subscription: base + '/subscriptions/:id',
+        subscription_notification: base + '/subscriptions/:subscription_id/notifications/:notification_id'
+      },
+      precision: config.get('amount.precision'),
+      scale: config.get('amount.scale')
+    }
+    this.body = metadata
+  }
+})
