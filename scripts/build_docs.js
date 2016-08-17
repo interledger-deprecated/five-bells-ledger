f438c1212ba695e9de6a4d9f95bbb52b7593420b
mDuo13 <mduo13@gmail.com>
2016-08-17 13:07:25 -0700

    doc buildscripts - follow lint rules

--- scripts/build_docs.js
+++ scripts/build_docs.js
@@ -7,19 +7,19 @@
  * copyright notice and this permission notice appear in all copies.
  */
 
-'use strict';
-const fs = require('fs');
-const path = require('path');
-//const execSync = require('child_process').execSync;
-const ejs = require('ejs');
+'use strict'
+const fs = require('fs')
+const path = require('path')
+// const execSync = require('child_process').execSync;
+const ejs = require('ejs')
 const renderFromPaths =
-  require('./json-schema-to-markdown-table2.js').renderFromPaths;
-const ROOT = path.dirname(path.normalize(__dirname));
+  require('./json-schema-to-markdown-table2.js').renderFromPaths
+const ROOT = path.dirname(path.normalize(__dirname))
 
-function strip(string) {
-  return string.replace(/^\s+|\s+$/g, '');
-}
-
+// function strip(string) {
+//   return string.replace(/^\s+|\s+$/g, '');
+// }
+//
 // function importFile(relativePath) {
 //   const absolutePath = path.join(ROOT, relativePath);
 //   return strip(fs.readFileSync(absolutePath).toString('utf-8'));
@@ -30,36 +30,36 @@ function strip(string) {
 //   return '\n```json\n' + json + '\n```\n';
 // }
 
-function renderSchema(schemaRelativePath) {
-  const schemasPath = path.join(ROOT, 'node_modules', 'five-bells-shared', 'schemas');
-  const schemaPath = path.join(schemasPath, schemaRelativePath);
-  return renderFromPaths(schemaPath, schemasPath);
+function renderSchema (schemaRelativePath) {
+  const schemasPath = path.join(ROOT, 'node_modules', 'five-bells-shared', 'schemas')
+  const schemaPath = path.join(schemasPath, schemaRelativePath)
+  return renderFromPaths(schemaPath, schemasPath)
 }
 
-function main() {
+function main () {
   const locals = {
 //    importFile: importFile,
 //    renderFixture: renderFixture,
     renderSchema: renderSchema
-  };
+  }
 
-  const introDocPath = path.join(ROOT, 'scripts', 'apidoc_intro.md.ejs');
-  ejs.renderFile(introDocPath, locals, function(error, output) {
+  const introDocPath = path.join(ROOT, 'scripts', 'apidoc_intro.md.ejs')
+  ejs.renderFile(introDocPath, locals, function (error, output) {
     if (error) {
-      console.error(error);
-      process.exit(1);
+      console.error(error)
+      process.exit(1)
     } else {
-      const outputPath = path.join(ROOT, 'apidoc-out', 'apidoc_intro.md');
+      const outputPath = path.join(ROOT, 'apidoc-out', 'apidoc_intro.md')
       try {
-          fs.mkdirSync('apidoc-out');
+        fs.mkdirSync('apidoc-out')
       } catch (e) {
-          if (e.code != 'EEXIST') throw e;
+        if (e.code !== 'EEXIST') throw e
       }
-      fs.writeFileSync(outputPath, output);
-      //execSync('npm run apidoc', {cwd: ROOT});
-      process.exit(0);
+      fs.writeFileSync(outputPath, output)
+      // execSync('npm run apidoc', {cwd: ROOT});
+      process.exit(0)
     }
-  });
+  })
 }
 
-main();
+main()
--- scripts/json-schema-to-markdown-table2.js
+++ scripts/json-schema-to-markdown-table2.js
@@ -6,224 +6,223 @@
  * copyright notice and this permission notice appear in all copies.
  */
 /* eslint no-use-before-define: [2, "nofunc"] */
-'use strict';
-const fs = require('fs');
-const join = require('path').join;
-const dirname = require('path').dirname;
+'use strict'
+const fs = require('fs')
+const join = require('path').join
+// const dirname = require('path').dirname
 
 // rows are represented as an array of three strings: name, type, description
-function includes(array, item) {
-  return array && array.indexOf(item) !== -1;
+function includes (array, item) {
+  return array && array.indexOf(item) !== -1
 }
 
-function flatten(arrays) {
-  return [].concat.apply([], arrays);
+function flatten (arrays) {
+  return [].concat.apply([], arrays)
 }
 
-function formatTable(rows) {
-  const header = ['Name', 'Type', 'Description'];
-  const divider = ['----', '----', '-----------'];
-  const allRows = [header, divider].concat(rows);
-  return allRows.map(row => row.join(' | ')).join('\n');
+function formatTable (rows) {
+  const header = ['Name', 'Type', 'Description']
+  const divider = ['----', '----', '-----------']
+  const allRows = [header, divider].concat(rows)
+  return allRows.map(row => row.join(' | ')).join('\n')
 }
 
-function formatName(path) {
+function formatName (path) {
   if (path.length === 0) {
-    return '';
+    return ''
   }
   if (path.length === 1) {
-    return path[0];
+    return path[0]
   }
-  return '*' + path.slice(0, -1).join('.') + '.* ' + path.slice(-1)[0];
+  return '*' + path.slice(0, -1).join('.') + '.* ' + path.slice(-1)[0]
 }
 
-function formatType(schema) {
+function formatType (schema) {
   if (schema.link) {
-    const prefix = includes(schema.link, '://') ? '' : '#';
-    return '[' + schema.title + '](' + prefix + schema.link + ')';
+    const prefix = includes(schema.link, '://') ? '' : '#'
+    return '[' + schema.title + '](' + prefix + schema.link + ')'
   }
   if (schema.format) {
-    return schema.format + ' string';
+    return schema.format + ' string'
   }
   if (schema.pattern) {
-    return 'string';
+    return 'string'
   }
   if (schema.enum) {
-    return 'string';
+    return 'string'
   }
-  return schema.type || 'object';
+  return schema.type || 'object'
 }
 
-function formatRow(schema, path, isRequired, typeOverride) {
-  const description = (isRequired ? '' : '*Optional* ')
-                    + (schema.description || '');
-  return [formatName(path), typeOverride || formatType(schema), description];
+function formatRow (schema, path, isRequired, typeOverride) {
+  const description = (isRequired ? '' : '*Optional* ') +
+                    (schema.description || '')
+  return [formatName(path), typeOverride || formatType(schema), description]
 }
 
-function sortKeys(keys, requiredKeys) {
-  const result = requiredKeys ? requiredKeys.slice() : [];
+function sortKeys (keys, requiredKeys) {
+  const result = requiredKeys ? requiredKeys.slice() : []
   result.forEach(key => {
     if (!includes(keys, key)) {
-      throw new Error('Property in "required" not in "properties": ' + key);
+      throw new Error('Property in "required" not in "properties": ' + key)
     }
-  });
+  })
   keys.sort().forEach(key => {
     if (!includes(result, key)) {
-      result.push(key);
+      result.push(key)
     }
-  });
-  return result;
+  })
+  return result
 }
 
-function generateRowsForObject(schema, path, schemas, isRequired) {
-  const keys = sortKeys(Object.keys(schema.properties), schema.required);
+function generateRowsForObject (schema, path, schemas, isRequired) {
+  const keys = sortKeys(Object.keys(schema.properties), schema.required)
   const rows = flatten(keys.map(name => {
-    const isRequiredField = includes(schema.required, name);
+    const isRequiredField = includes(schema.required, name)
     return generateRowsForSchema(
-      schema.properties[name], path.concat([name]), schemas, isRequiredField);
-  }));
-  return path.length > 0 ?
-    [formatRow(schema, path, isRequired)].concat(rows) : rows;
+      schema.properties[name], path.concat([name]), schemas, isRequiredField)
+  }))
+  return path.length > 0 ? [formatRow(schema, path, isRequired)].concat(rows) : rows
 }
 
-function generateRowsForArray(schema, path, schemas, isRequired) {
-  const newPath = path.slice(0, -1).concat([path.slice(-1)[0] + '[]']);
-  const rows = generateRowsForSchema(schema.items, newPath, schemas, true);
+function generateRowsForArray (schema, path, schemas, isRequired) {
+  const newPath = path.slice(0, -1).concat([path.slice(-1)[0] + '[]'])
+  const rows = generateRowsForSchema(schema.items, newPath, schemas, true)
   if (rows.length === 1) {
-    const typeOverride = 'array\\[' + rows[0][1] + '\\]';
-    return [formatRow(schema, path, isRequired, typeOverride)];
+    const typeOverride = 'array\\[' + rows[0][1] + '\\]'
+    return [formatRow(schema, path, isRequired, typeOverride)]
   }
-  const firstRow = formatRow(schema, path, isRequired);
-  return [firstRow].concat(rows);
+  const firstRow = formatRow(schema, path, isRequired)
+  return [firstRow].concat(rows)
 }
 
-function removeDuplicates(rows) {
-  const hash = {};
-  const result = [];
+function removeDuplicates (rows) {
+  const hash = {}
+  const result = []
   rows.forEach(row => {
-    const key = row.join('|');
+    const key = row.join('|')
     if (!hash[key]) {
-      result.push(row);
-      hash[key] = true;
+      result.push(row)
+      hash[key] = true
     }
-  });
-  return result;
+  })
+  return result
 }
 
-function overrideDescription(schema, description) {
-  const override = description ? {description: description} : {};
-  return assign(assign({}, schema), override);
+function overrideDescription (schema, description) {
+  const override = description ? {description: description} : {}
+  return assign(assign({}, schema), override)
 }
 
-function generateRowsForBranch(branchSchemas, path, description, schemas,
+function generateRowsForBranch (branchSchemas, path, description, schemas,
     isRequired) {
-  const nonNullSchemas = branchSchemas.filter(schema => schema.type !== 'null');
+  const nonNullSchemas = branchSchemas.filter(schema => schema.type !== 'null')
   const rows = flatten(nonNullSchemas.map(branchSchema =>
     generateRowsForSchema(overrideDescription(branchSchema, description),
-      path, schemas, isRequired)));
-  const result = removeDuplicates(rows);
-  const hasNull = (nonNullSchemas.length < branchSchemas.length);
+      path, schemas, isRequired)))
+  const result = removeDuplicates(rows)
+  const hasNull = (nonNullSchemas.length < branchSchemas.length)
   if (hasNull) {
     for (var i = 0; i < result.length; i++) {
-      result[i][1] = result[i][1] + ', null';
+      result[i][1] = result[i][1] + ', null'
     }
   }
-  return result;
+  return result
 }
 
-function generateRowsForCompleteSchema(schema, path, schemas, isRequired) {
+function generateRowsForCompleteSchema (schema, path, schemas, isRequired) {
   if (schema.link && path.length > 0) {
-    return [formatRow(schema, path, isRequired)];
+    return [formatRow(schema, path, isRequired)]
   }
   if (schema.type === 'array') {
     if (path.length > 0) {
-      return generateRowsForArray(schema, path, schemas, isRequired);
+      return generateRowsForArray(schema, path, schemas, isRequired)
     }
-    return generateRowsForSchema(schema.items, path, schemas, true);
+    return generateRowsForSchema(schema.items, path, schemas, true)
   }
   if (schema.properties) {
-    return generateRowsForObject(schema, path, schemas, isRequired);
+    return generateRowsForObject(schema, path, schemas, isRequired)
   }
   if (schema.additionalProperties) {
     return generateRowsForSchema(schema.additionalProperties,
-      path.concat('\\*'), schemas, isRequired);
+      path.concat('\\*'), schemas, isRequired)
   }
   if (schema.oneOf) {
     return generateRowsForBranch(schema.oneOf, path, schema.description,
-      schemas, isRequired);
+      schemas, isRequired)
   }
   if (schema.anyOf) {
     return generateRowsForBranch(schema.anyOf, path, schema.description,
-      schemas, isRequired);
+      schemas, isRequired)
   }
-  return [formatRow(schema, path, isRequired)];
+  return [formatRow(schema, path, isRequired)]
 }
 
-function assign(destination, source) {
+function assign (destination, source) {
   for (let key in source) { // eslint-disable-line
     if (source.hasOwnProperty(key)) {
-      destination[key] = source[key];
+      destination[key] = source[key]
     }
   }
-  return destination;
+  return destination
 }
 
-function completeSchema(schema, schemas) {
+function completeSchema (schema, schemas) {
   if (schema.$ref) {
-    const refSchema = schemas[schema.$ref];
+    const refSchema = schemas[schema.$ref]
     if (!refSchema) {
-      throw new Error('Could not find schema for: ' + schema.$ref);
+      throw new Error('Could not find schema for: ' + schema.$ref)
     }
-    return assign(assign({}, refSchema), schema);
+    return assign(assign({}, refSchema), schema)
   }
-  return schema;
+  return schema
 }
 
-function generateRowsForSchema(schema, path, schemas, isRequired) {
-  const completedSchema = completeSchema(schema, schemas);
+function generateRowsForSchema (schema, path, schemas, isRequired) {
+  const completedSchema = completeSchema(schema, schemas)
   return generateRowsForCompleteSchema(completedSchema, path, schemas,
-    isRequired);
+    isRequired)
 }
 
-function recursivelyListDirectory(directory) {
-  const filenames = fs.readdirSync(directory);
-  let results = [];
+function recursivelyListDirectory (directory) {
+  const filenames = fs.readdirSync(directory)
+  let results = []
   filenames.forEach(filename => {
-    const filepath = join(directory, filename);
-    const stat = fs.statSync(filepath);
+    const filepath = join(directory, filename)
+    const stat = fs.statSync(filepath)
     if (stat && stat.isDirectory()) {
-      results = results.concat(recursivelyListDirectory(filepath));
+      results = results.concat(recursivelyListDirectory(filepath))
     } else {
-      results.push([filename, filepath]);
+      results.push([filename, filepath])
     }
-  });
-  return results;
+  })
+  return results
 }
 
-function loadSchema(filepath) {
-  return JSON.parse(fs.readFileSync(filepath));
+function loadSchema (filepath) {
+  return JSON.parse(fs.readFileSync(filepath))
 }
 
-function loadSchemas(schemaDirectory) {
-  const filepaths = recursivelyListDirectory(schemaDirectory);
-  const schemas = {};
+function loadSchemas (schemaDirectory) {
+  const filepaths = recursivelyListDirectory(schemaDirectory)
+  const schemas = {}
   filepaths.forEach(fpair => {
-    let [filename, filepath] = fpair;
+    let [filename, filepath] = fpair
     if (filepath.endsWith('.json')) {
-      const schema = loadSchema(filepath);
-      schemas[filename] = schema;
+      const schema = loadSchema(filepath)
+      schemas[filename] = schema
     }
-  });
-  return schemas;
+  })
+  return schemas
 }
 
-function render(schema, schemas) {
-  return formatTable(generateRowsForSchema(schema, [], schemas || {}, true));
+function render (schema, schemas) {
+  return formatTable(generateRowsForSchema(schema, [], schemas || {}, true))
 }
 
-function renderFromPaths(schemaPath, schemasPath) {
-  return render(loadSchema(schemaPath), loadSchemas(schemasPath));
+function renderFromPaths (schemaPath, schemasPath) {
+  return render(loadSchema(schemaPath), loadSchemas(schemasPath))
 }
 
-module.exports.render = render;
-module.exports.renderFromPaths = renderFromPaths;
+module.exports.render = render
+module.exports.renderFromPaths = renderFromPaths
