6f47ce4e9bf1edca01dec52c76984e0ff999552d
sentientwaffle <sentientwaffle@gmail.com>
2016-10-31 09:26:06 -0700

    feat: allow admin to send message as any user
    Fixes #346
    
    Depends on https://github.com/interledgerjs/five-bells-shared/pull/160

--- src/models/messages.js
+++ src/models/messages.js
@@ -14,8 +14,21 @@ function * sendMessage (message, requestingUser) {
     throw new InvalidBodyError(error, validationResult.errors)
   }
 
-  const recipientName = uri.parse(message.account, 'account').name.toLowerCase()
-  const senderAccount = uri.make('account', requestingUser.name)
+  const mode1 = !!message.account
+  const mode2 = !!(message.from && message.to)
+  if ((mode1 && mode2) || (!mode1 && !mode2)) {
+    throw new InvalidBodyError('Expected either message.account OR message.from/to')
+  }
+
+  const senderAccount = message.from || uri.make('account', requestingUser.name)
+  const senderName = uri.parse(senderAccount, 'account').name.toLowerCase()
+  const recipientName = uri.parse(message.to || message.account, 'account').name.toLowerCase()
+
+  // Only admin can impersonate users.
+  if (!requestingUser.is_admin && senderName !== requestingUser.name) {
+    throw new InvalidBodyError('You do not have permission to impersonate this user')
+  }
+
   yield notificationBroadcaster.sendMessage(recipientName, {
     ledger: message.ledger,
     account: senderAccount,
