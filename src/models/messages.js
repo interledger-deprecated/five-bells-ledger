a8e63713d75df19c767042f0973c0561cdcf65f6
Stefan Thomas <justmoon@members.fsf.org>
2016-11-09 10:39:36 -0800

    feat: log relayed messages

--- src/models/messages.js
+++ src/models/messages.js
@@ -4,6 +4,7 @@ const InvalidBodyError = require('five-bells-shared/errors/invalid-body-error')
 const uri = require('../services/uriManager')
 const validator = require('../services/validator')
 const notificationBroadcaster = require('../services/notificationBroadcaster')
+const log = require('../services/log').create('messages')
 
 function * sendMessage (message, requestingUser) {
   const validationResult = validator.create('Message')(message)
@@ -23,6 +24,8 @@ function * sendMessage (message, requestingUser) {
   const senderAccount = message.from
   const senderName = uri.parse(senderAccount, 'account').name.toLowerCase()
   const recipientName = uri.parse(message.to, 'account').name.toLowerCase()
+
+  log.debug('%s -> %s: %o', senderName, recipientName, message.data)
 
   // Only admin can impersonate users.
   if (!requestingUser.is_admin && senderName !== requestingUser.name) {
