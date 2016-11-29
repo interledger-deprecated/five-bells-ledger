b2dc22d8ea8b9d506d837cd47748219e4df9004f
CircleCI <info@circleci.com>
2016-11-29 19:19:22 +0000

    chore: update gh-pages

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "five-bells-ledger",
-  "version": "19.2.1",
+  "version": "19.2.2",
   "description": "Five Bells ledger reference implementation",
   "title": "Interledger Reference Ledger API",
   "url": "https://acme-ledger.example",
@@ -26,7 +26,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-11-29T19:17:48.143Z",
+    "time": "2016-11-29T19:19:21.074Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
