d4bab8f41e20d65d126a4ce718f0272beb637b54
Stefan Thomas <justmoon@members.fsf.org>
2017-02-17 21:32:45 +0100

    feat: remove byExecutionCondition endpoint

--- test/getTransferSpec.js
+++ test/getTransferSpec.js
@@ -3,7 +3,6 @@
 const _ = require('lodash')
 const nock = require('nock')
 nock.enableNetConnect(['localhost', '127.0.0.1'])
-const expect = require('chai').expect
 const app = require('../src/services/app')
 const logger = require('../src/services/log')
 const dbHelper = require('./helpers/db')
@@ -96,119 +95,6 @@ describe('GET /transfers/:uuid', function () {
         }
       }))
       .expect(validator.validateTransfer)
-      .end()
-  })
-})
-
-describe('GET /transfers/byExecutionCondition/:execution_condition', function () {
-  logHelper(logger)
-
-  before(function * () {
-    yield dbHelper.init()
-  })
-
-  beforeEach(function * () {
-    appHelper.create(this, app)
-    yield dbHelper.clean()
-    this.clock = sinon.useFakeTimers(START_DATE, 'Date')
-
-    // Define example data
-    this.executedTransfer = _.cloneDeep(require('./data/transfers/executed'))
-    this.transferWithExpiry = _.cloneDeep(require('./data/transfers/withExpiry'))
-    this.transferWithAndCondition = _.cloneDeep(require('./data/transfers/withAndCondition'))
-    this.preparedTransfer = _.cloneDeep(require('./data/transfers/prepared'))
-
-    // Store some example data
-    yield dbHelper.addAccounts(_.values(require('./data/accounts')))
-    yield dbHelper.addTransfers([this.executedTransfer])
-  })
-
-  afterEach(function * () {
-    nock.cleanAll()
-    this.clock.restore()
-  })
-
-  it('should return 200 for an existing transfer', function * () {
-    const transfer = this.executedTransfer
-    yield this.request()
-      .get(`/transfers/byExecutionCondition/${transfer.execution_condition}`)
-      .expect(200)
-      .expect([transfer])
-      .expect(validator.validateTransfers)
-      .end()
-  })
-
-  it('should return 200 for multiple existing transfers', function * () {
-    yield dbHelper.addTransfers([this.preparedTransfer])
-
-    const transfer = this.executedTransfer
-    yield this.request()
-      .get(`/transfers/byExecutionCondition/${transfer.execution_condition}`)
-      .expect(200)
-      .expect((res) => {
-        const sortedResponse = _.sortBy(res.body, (transfer) => {
-          return transfer.id
-        })
-        const sortedTransfers = _.sortBy([transfer, this.preparedTransfer], (transfer) => {
-          return transfer.id
-        })
-        expect(sortedResponse).to.deep.equal(sortedTransfers)
-      })
-      .expect(validator.validateTransfers)
-      .end()
-  })
-
-  it('should return 404 when the transfer does not exist', function * () {
-    yield this.request()
-      .get(`/transfers/byExecutionCondition/${this.transferWithAndCondition.execution_condition}`)
-      .expect(404)
-      .expect(function (res) {
-        expect(res.body.id).to.equal('NotFoundError')
-        expect(res.body.message).to.equal('Unknown execution condition')
-      })
-      .end()
-  })
-
-  it('should return 400 when the provided parameter is not an execution condition', function * () {
-    yield this.request()
-      .get('/transfers/byExecutionCondition/notanexecutioncondition')
-      .expect(400)
-      .expect(function (res) {
-        expect(res.body.id).to.equal('InvalidUriParameterError')
-        expect(res.body.message).to.equal('execution_condition is not a valid Condition')
-      })
-      .end()
-  })
-
-  it('should return a rejected transfer if the expiry date has passed', function * () {
-    const transfer = this.transferWithExpiry
-    delete transfer.debits[0].authorized
-    delete transfer.debits[1].authorized
-
-    yield this.request()
-      .put(transfer.id)
-      .auth('alice', 'alice')
-      .send(transfer)
-      .expect(201)
-      .expect(validator.validateTransfer)
-      .end()
-
-    this.clock.tick(1000)
-
-    // In production this function should be triggered by the worker started in app.js
-    yield transferExpiryMonitor.processExpiredTransfers()
-
-    yield this.request()
-      .get(`/transfers/byExecutionCondition/${transfer.execution_condition}`)
-      .expect(200, [_.assign({}, transfer, {
-        state: transferStates.TRANSFER_STATE_REJECTED,
-        rejection_reason: 'expired',
-        timeline: {
-          proposed_at: '2015-06-16T00:00:00.000Z',
-          rejected_at: transfer.expires_at
-        }
-      })])
-      .expect(validator.validateTransfers)
       .end()
   })
 })
