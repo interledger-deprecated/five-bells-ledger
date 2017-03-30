e406e6a25e8da7f8080c51fd4f969359b1f3ae0c
Dennis Appelt <dappelt@ripple.com>
2017-03-30 16:34:24 +0200

    fix: fixes #415

--- src/lib/updateState.js
+++ src/lib/updateState.js
@@ -3,12 +3,13 @@
 const moment = require('moment')
 const log = require('../services/log').create('updateState')
 
-function updateState (transfer, state) {
+function updateState (transfer, state, opts) {
   log.debug('updating transfer state from ' + transfer.state +
     ' to ' + state)
 
   transfer.state = state
-  transfer[state + '_at'] = moment().toISOString()
+  const updatedAt = opts && opts.updatedAt || moment()
+  transfer[state + '_at'] = updatedAt.toISOString()
 }
 
 module.exports = updateState
